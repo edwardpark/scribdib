09a4d312bd5148ca67b0eaa09c8941491f342d7e
Erica Andersen <andersenek@gmail.com>
2015-07-28 23:40:46 -0400

    Added views and controllers (full CRUD/REST) for posts, Added helpers and forms, Added strong params.

---
+++ scribble/app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post) # Knows to get the id of this post
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private # Anything defined under private is not a CRUD action and is just a helper to use in instances.
+    def post_params
+      params.require(:post).permit(:title, :content, :photo_url)
+    end
+
+end
