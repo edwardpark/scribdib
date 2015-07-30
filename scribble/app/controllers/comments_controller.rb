6fbdcb34a970b20d1ea101c3526967084cdfaf9d
Erica Andersen <andersenek@gmail.com>
2015-07-29 23:53:22 -0400

    Added comment 'show' functionality.

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    # @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    # @post = Post.find(params[:id])
+    @comment = Comment.create!(comment_params)
+    redirect_to user_comment_path(@user, @comment)
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:subject, :comment, :post_id, :user_id)
+  end
 end
--- scribble/app/controllers/posts_controller.rb
+++ scribble/app/controllers/posts_controller.rb
@@ -4,7 +4,9 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     @post = Post.find(params[:id])
+    @comments = Comment.all
   end
 
   def new
